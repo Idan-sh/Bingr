938f171ebd9952fce294cffdeea051a2b7c296f1
Idan <104992892+Idan-sh@users.noreply.github.com>
2025-01-07 20:24:50 +0200

    Added animations to landing page hero component.

--- frontend/src/components/landing-page/LandingPageHero.tsx
+++ frontend/src/components/landing-page/LandingPageHero.tsx
@@ -1,7 +1,10 @@
-import { Typography, Button, Container } from "@mui/material";
+import { Typography, Button, Container, Box } from "@mui/material";
 import { motion } from "motion/react";
 
 function LandingPageHero() {
+  const delayInterval = 0.5;
+  const distance = 100;
+
   return (
     <Container
       className="landing-page"
@@ -13,37 +16,75 @@ function LandingPageHero() {
         padding: 4,
         textAlign: "center",
         height: "calc(100vh - 3.9rem)",
-        width: "80%",
-        paddingTop: "4rem"
+        paddingTop: "2rem"
       }}
     >
+      <Box width="90%" flexDirection="column">
+        <motion.div
+          initial={{ opacity: 0, x: distance }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: delayInterval * 0 }}
+        >
+          <Typography variant="h2" textAlign="end" gutterBottom>
+            Swipe Right
+          </Typography>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: -distance }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: delayInterval * 1 }}
+        >
+          <Typography variant="h2" textAlign="start" gutterBottom>
+            to Your
+          </Typography>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: distance }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: delayInterval * 2 }}
+        >
+          <Typography variant="h2" textAlign="center" gutterBottom>
+            Next
+          </Typography>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: -distance }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: delayInterval * 3 }}
+        >
+          <Typography variant="h2" textAlign="end" gutterBottom>
+            Binge-Worthy
+          </Typography>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: distance }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: delayInterval * 4 }}
+        >
+          <Typography variant="h2" textAlign="center" gutterBottom>
+            Adventure
+          </Typography>
+        </motion.div>
+      </Box>
       <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: delayInterval * 6 }}
       >
-        <Typography variant="h2" gutterBottom>
-          Swipe Right
+        <Typography variant="h6" sx={{ marginTop: "3rem" }}>
+          Your personal matchmaker for movies and TV shows!
         </Typography>
-        <Typography variant="h2" gutterBottom>
-          to Your Next
+        <Typography variant="h6" sx={{ marginBottom: "2rem" }}>
+          Swipe your way to the perfect entertainment, tailored just for you.
         </Typography>
-        <Typography variant="h2" gutterBottom>
-          Binge-Worthy
-        </Typography>
-        <Typography variant="h2" gutterBottom>
-          Adventure
-        </Typography>
+        <Button variant="contained" color="primary" size="large">
+          Get Started
+        </Button>
       </motion.div>
-      <Typography variant="h6" sx={{ marginTop: "3rem" }}>
-        Your personal matchmaker for movies and TV shows!
-      </Typography>
-      <Typography variant="h6" sx={{ marginBottom: "2rem" }}>
-        Swipe your way to the perfect entertainment, tailored just for you.
-      </Typography>
-      <Button variant="contained" color="primary" size="large">
-        Get Started
-      </Button>
     </Container>
   );
 }
