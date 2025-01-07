8a3c1cb0485563198a2548faca25521cb6ea4867
Idan <104992892+Idan-sh@users.noreply.github.com>
2025-01-07 14:34:20 +0200

    Added font to the index HTML.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import { motion } from "framer-motion";
 import "/src/assets/styles/pages/landing-page.scss";
 import LandingPageHero from "../components/landing-page/LandingPageHero";
 import LandingPageTrending from "../components/landing-page/LandingPageTrending";
