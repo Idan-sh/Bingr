b88c19b3c9c039c06eed20ca8331c1bf534e4a35
ronenii <ronen.glm@gmail.com>
2025-01-17 17:29:29 +0200

    Added whitelist for  body parameters

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,8 +8,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
 
+  // Enable validators for body parameters
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Ignore body parameters not in DTOs
+    }),
+  );
+
+  // COnfigure swagger
   const config = new DocumentBuilder()
     .setTitle('Bingr')
     .setDescription('Bingr API documentation')
